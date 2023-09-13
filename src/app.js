06be2a4ff665197631be9c9c0e0d86b13608eb40
gonchip <136641475+gonchip@users.noreply.github.com>
2023-09-13 19:59:44 +0000

    Card creado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,44 @@
 import "./style.css";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  const numberElement = document.querySelector(".number");
+
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Q",
+    "K",
+    "J",
+    "A"
+  ];
+
+  const randomIndexValue = Math.floor(Math.random() * cardValues.length);
+
+  const selectedValue = cardValues[randomIndexValue];
+
+  numberElement.textContent = selectedValue;
+
+  const topElement = document.querySelector(".top");
+  const bottomElement = document.querySelector(".bottom");
+
+  const cardSymbols = ["♠", "♥", "♦", "♣"];
+
+  const cardColors = ["black", "red", "red", "black"];
+
+  const randomIndexSymbol = Math.floor(Math.random() * cardSymbols.length);
+
+  const selectedSymbol = cardSymbols[randomIndexSymbol];
+  const selectedColor = cardColors[randomIndexSymbol];
+
+  topElement.textContent = selectedSymbol;
+  bottomElement.textContent = selectedSymbol;
+  topElement.style.color = selectedColor;
+  bottomElement.style.color = selectedColor;
 };
